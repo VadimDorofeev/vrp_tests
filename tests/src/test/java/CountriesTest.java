e8f5b5e595a34c3e411e3b5ea25162a848d610c3
VadimDorofeev <vdmdorofeev@gmail.com>
2021-01-29 17:39:30 +0300

    Added progect

---
+++ tests/src/test/java/CountriesTest.java
@@ -0,0 +1,87 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class CountriesTest {
+
+    WebDriver driver = new FirefoxDriver();
+
+    @BeforeTest
+    void init() {
+        System.setProperty("webdriver.geco.driver", "gecodriver.exe");
+        driver.get("http://localhost/litecart/admin");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    void test() {
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@name='login']")).click();
+        driver.findElement(By.xpath("//a[contains(@href,'countries')]")).click();
+
+        List<WebElement> countries = driver.findElements(By.className("row"));
+        List<String> namesOfTheCountries = new ArrayList<>();
+
+        for (WebElement country : countries) {
+            String nameOfTheCountry = country.findElement(By.xpath("./*[5]//a")).getAttribute("text");
+            namesOfTheCountries.add(nameOfTheCountry);
+            String zones = country.findElement(By.xpath("./td[6]")).getText();
+
+            if (!zones.equals("0")) {
+
+                country.findElement(By.xpath("./*[5]//a")).click();
+                List <WebElement> zonesList = driver.findElements(By.xpath("//input[contains(@type,'hidden') and contains(@name,'name')]"));
+                List <String> namesOfTheZones = new ArrayList<>();
+
+                for (WebElement zone : zonesList) {
+                    String nameOfTheZone = zone.findElement(By.xpath("//input[contains(@type,'hidden') and contains(@name,'name')]")).
+                            getAttribute("innerText");
+                    namesOfTheZones.add(nameOfTheZone);
+                }
+                List <String> sortedNamesOfTheZones = namesOfTheZones.stream().sorted().collect(Collectors.toList());
+
+                for (int i = 0; i < namesOfTheZones.size(); i++) {
+                    Assert.assertTrue(sortedNamesOfTheZones.get(i).equals(namesOfTheZones.get(i)));
+                }
+
+                driver.findElement(By.xpath("//button[@name='cancel']")).click();
+
+                for (String printZones: namesOfTheZones) {
+                    System.out.println(printZones);
+                }
+
+                System.out.println(namesOfTheZones.size());
+                System.out.println(sortedNamesOfTheZones.size());
+            }
+        }
+
+        List<String> sortedNamesOfElements = namesOfTheCountries.stream().sorted().collect(Collectors.toList());
+
+        for (int i = 0; i < sortedNamesOfElements.size(); i++) {
+            Assert.assertTrue(sortedNamesOfElements.get(i).equals(namesOfTheCountries.get(i)));
+        }
+
+        for (String print : namesOfTheCountries) {
+            System.out.println(print);
+        }
+
+        System.out.println(namesOfTheCountries.size());
+        System.out.println(sortedNamesOfElements.size());
+    }
+
+    @AfterTest
+    void after() {
+        driver.quit();
+    }
+}
---
+++ tests/src/test/java/StickerTest.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class StickerTest {
+    WebDriver driver = new FirefoxDriver();
+
+    @BeforeTest
+    void init() {
+        System.setProperty("webdriver.geco.driver","gecodriver.exe");
+        driver.get("http://localhost/litecart/");
+    }
+
+    @Test
+    void test() {
+        driver.findElement(By.className("category-1")).click();
+        List<WebElement> goods = driver.findElements(By.className("product column shadow hover-light"));
+        for (WebElement oneGood: goods) {
+            String sticker = oneGood.findElement(By.xpath("./contains([@class,'sticker'")).getText();
+            Assert.assertTrue(sticker.equals("New") || sticker.equals("Sale"));
+        }
+    }
+
+    @AfterTest
+    void stop() {
+        driver.quit();
+    }
+}
